556cb66e6252a357262440d9bc940c88d568789c
Yutaka <y.tajika529@gmail.com>
2022-04-01 10:35:43 +0900

    Initial Commit

---
+++ DangerSwiftPeripherySampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DangerSwiftPeripherySampleApp
+//
+//  Created by 多鹿豊 on 2022/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
