d8c7eadbb916cda855c9d099977c0c29e6fd11b3
Yutaka <y.tajika529@gmail.com>
2022-08-03 21:52:30 +0900

    handle manually

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -1,7 +1,16 @@
 import Danger
 import DangerSwiftPeriphery
 
-DangerPeriphery.scan {
+let result = DangerPeriphery.scan(arguments: {
     PeripheryArguments.reportExclude(["Sources/DangerSwiftPeripherySampleApp/DangerSwiftPeripherySampleApp.swift"])
     PeripheryArguments.cleanBuild
+}, shouldComment: false)
+
+switch result {
+case let .success(warnings):
+    if let warning = warnings.first {
+        warn(message: warning.message, file: warning.file, line: warning.line)
+    }
+case let .failure(error):
+    fail(error.localizedDescription)
 }
