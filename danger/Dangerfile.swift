a033a8b14b700be6d81b20b8b4cfbf537500f255
Yutaka <y.tajika529@gmail.com>
2022-08-13 23:14:55 +0900

    use fix-ambiguous-interface

--- danger/Dangerfile.swift
+++ danger/Dangerfile.swift
@@ -1,4 +1,16 @@
 import Danger
 import DangerSwiftPeriphery
 
-DangerPeriphery.scan()
+let result = DangerPeriphery.scan(arguments: {
+    PeripheryArguments.reportExclude(["Sources/DangerSwiftPeripherySampleApp/DangerSwiftPeripherySampleApp.swift"])
+    PeripheryArguments.cleanBuild
+}, shouldComment: false)
+
+switch result {
+case let .success(warnings):
+    for warning in warnings {
+        warn(message: warning.message, file: warning.filePath, line: warning.line)
+    }
+case let .failure(error):
+    fail(error.localizedDescription)
+}
