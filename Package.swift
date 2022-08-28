23f7bcd6f5fb20a118cd6ca3d22d4d1663f6beef
Yutaka <y.tajika529@gmail.com>
2022-08-28 14:04:51 +0900

    use improve-shell-executor-test

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Danger
         .package(url: "https://github.com/danger/swift.git", from: "3.0.0"), // dev
         // Danger Plugins
-        .package(url: "https://github.com/taji-taji/DangerSwiftPeriphery.git", branch: "introduce-danger-swift-test"), // dev
+        .package(url: "https://github.com/taji-taji/DangerSwiftPeriphery.git", branch: "improve-shell-executor-test"), // dev
     ],
     targets: [
         .target(name: "DangerSwiftPeripherySampleApp"),
